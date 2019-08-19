ba02f67652b817f83860ffb73e04de041adbb52a
huanghaitao <A10696@tvt.net.cn>
2019-08-19 20:22:58 +0800

    装饰者模式

--- decoratorPattern/src/com/ecjtu/hht/TestDecoratorPattern.java
+++ decoratorPattern/src/com/ecjtu/hht/TestDecoratorPattern.java
@@ -1,5 +1,10 @@
 package com.ecjtu.hht;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.OutputStream;
+
 /**
  * @author hht
  * @date 2019/7/27 17:13
@@ -8,14 +13,15 @@ package com.ecjtu.hht;
 public class TestDecoratorPattern {
     public static void main(String[] args) {
         //1创建装饰者对象  ：一杯什么都不加的奶茶
-        Tea milkTea = new MilkTea();
-        System.out.println(milkTea.getDescription() + "," + milkTea.cost());
+        //Tea milkTea = new MilkTea();
+        //System.out.println(milkTea.getDescription() + "," + milkTea.cost());
         //加珍珠
-        milkTea = new CoconutIngredientsDecorator(milkTea);
-        System.out.println(milkTea.getDescription() + "," + milkTea.cost());
+        // milkTea = new CoconutIngredientsDecorator(milkTea);
+        // System.out.println(milkTea.getDescription() + "," + milkTea.cost());
         //加椰果
-        milkTea=new PearlIngredientsDecorator(milkTea);
-        System.out.println(milkTea.getDescription() + "," + milkTea.cost());
+        //milkTea=new PearlIngredientsDecorator(milkTea);
+        Tea tea = new CoconutIngredientsDecorator(new PearlIngredientsDecorator(new MilkTea()));
+        System.out.println(tea.getDescription() + "," + tea.cost());
 
     }
 }
